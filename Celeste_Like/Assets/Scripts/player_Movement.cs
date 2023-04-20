1f6d86147b61e25bd0832389ca4c73252e2108c1
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-04-19 23:27:25 -0300

    Starting vertical movement
    Creating the basics of the jump; And adding a function to handle the fall.
    Also changed the size of the main character.

--- Celeste_Like/Assets/Scripts/player_Movement.cs
+++ Celeste_Like/Assets/Scripts/player_Movement.cs
@@ -14,7 +14,7 @@ public class player_Movement : MonoBehaviour
 
     //Vertical movement
     private bool canJump = false;
-    private float jumpPower = 20f;
+    private float jumpPower = 10f;
     private float maxFallSpeed = 20f; //when the player is in maxFallSpeed change settings to avoid the need of changing the velocity every time
     private float fallSpeedIncrease = 5f;
 
@@ -24,15 +24,32 @@ public class player_Movement : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontalDirection = Input.GetAxisRaw("Horizontal");
-        verticalDirection = Input.GetAxisRaw("Vertical"); 
-
+        verticalDirection = Input.GetAxisRaw("Vertical");
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            performJump();
+        }
+        if(rb.velocity.y < 0)
+        {
+            
+        }
     }
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontalDirection*horizontalSpeed,rb.velocity.y);
+    }
+
+    private void performJump()//Do some checks in here to assure that it can jump
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+    }
+
+    private void fallHander()
+    {
+        //handle all the stuff related to fall, like maxFallSpeed, Falling increase, and so on
     }
 }
