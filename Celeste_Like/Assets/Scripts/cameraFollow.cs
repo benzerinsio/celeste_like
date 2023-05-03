aec7d0a8b57836a3d576f6cca7e929333f6a2a81
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-05-03 01:38:50 -0300

    Trying to make camera more dinamic

--- Celeste_Like/Assets/Scripts/cameraFollow.cs
+++ Celeste_Like/Assets/Scripts/cameraFollow.cs
@@ -6,11 +6,13 @@ public class cameraFollow : MonoBehaviour
 {
 
     [SerializeField] private Transform player;
+    [SerializeField] private Rigidbody2D playerRb;
     private float correctYpos = 1.75f;
     public float followSpeed = 10f;
+
     void Update()
     {
-        Vector3 newPos = new Vector3(player.position.x, player.position.y + correctYpos,-10f);
+        Vector3 newPos = new Vector3(player.position.x + playerRb.velocity.x, player.position.y + correctYpos + playerRb.velocity.y, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
         //transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
