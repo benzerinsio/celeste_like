e50aa9fafe2f97b6c915620e8c11eda3f87d8d37
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-05-03 13:16:38 -0300

    Camera behaviour and map finishing

--- Celeste_Like/Assets/Scripts/cameraFollow.cs
+++ Celeste_Like/Assets/Scripts/cameraFollow.cs
@@ -8,11 +8,11 @@ public class cameraFollow : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Rigidbody2D playerRb;
     private float correctYpos = 1.75f;
-    public float followSpeed = 10f;
+    public float followSpeed = 15f;
 
     void Update()
     {
-        Vector3 newPos = new Vector3(player.position.x + playerRb.velocity.x, player.position.y + correctYpos + playerRb.velocity.y, -10f);
+        Vector3 newPos = new Vector3(player.position.x + (playerRb.velocity.x /3), player.position.y + correctYpos + (playerRb.velocity.y /3.5f), -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
         //transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
