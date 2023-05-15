6036ee435552285f60af3ec5b9cd84d00763a08f
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-05-15 16:47:40 -0300

    Continuing first level
    Putting all the traps in the first level for the user to learn its behaviour in the future levels and also know how far it can go. Also removing the collider from the Saw.

--- Celeste_Like/Assets/Scripts/sawBehaviour.cs
+++ Celeste_Like/Assets/Scripts/sawBehaviour.cs
@@ -6,19 +6,27 @@ public class sawBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private GameObject[] waypoints;
+    [SerializeField] Player_Life die;
     private int currentPoint = 0;
     private float planSpeed = 6f;
     private void FixedUpdate()
     {
-            if (Vector2.Distance(waypoints[currentPoint].transform.position, transform.position) < .1f)
+        if (Vector2.Distance(waypoints[currentPoint].transform.position, transform.position) < .1f)
+        {
+            currentPoint++;
+            if (currentPoint >= waypoints.Length)
             {
-                currentPoint++;
-                if (currentPoint >= waypoints.Length)
-                {
-                    currentPoint = 0;
-                }
-
+                currentPoint = 0;
             }
-            transform.position = Vector2.MoveTowards(transform.position, waypoints[currentPoint].transform.position, Time.deltaTime * planSpeed);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentPoint].transform.position, Time.deltaTime * planSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            die.dieHandler();
+        }
     }
 }
