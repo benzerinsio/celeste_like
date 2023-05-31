d2baca26f40574e2aae29b15d4c767669f23e743
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-05-31 20:49:48 -0300

    Finishing the static shooting trap
    Adding the final sprite of the static shooting trap. Also animating it and adjusting its script to fit the right time to shoot.

--- Celeste_Like/Assets/Scripts/ShootBehaviour.cs
+++ Celeste_Like/Assets/Scripts/ShootBehaviour.cs
@@ -6,25 +6,6 @@ public class ShootBehaviour : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject bulletPrefab;
-    private float shootTimer = 1f;
-    private float shootCounter;
-
-
-    private void Start()
-    {
-        shootCounter = shootTimer;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        shootCounter -= Time.deltaTime;
-
-        if (shootCounter <= 0f)
-        {
-            shootCounter = shootTimer;
-            Shoot();
-        }
-    }
 
     void Shoot()
     {
