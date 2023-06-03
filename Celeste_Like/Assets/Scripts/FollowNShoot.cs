bd7f9943fd733e300601fbedab60d64fd98e8719
Felipe Vinicius de Sousa <31699547+benzerinsio@users.noreply.github.com>
2023-06-02 23:32:38 -0300

    Finishing the behaviour of last trap
    The follow shooting trap only follows the player if its in range.

--- Celeste_Like/Assets/Scripts/FollowNShoot.cs
+++ Celeste_Like/Assets/Scripts/FollowNShoot.cs
@@ -7,6 +7,7 @@ public class FollowNShoot : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject bulletPrefab;
+    private bool playerClose = false;
     private Rigidbody2D rb;
 
     private void Start()
@@ -15,6 +16,24 @@ public class FollowNShoot : MonoBehaviour
     }
 
     private void Update()
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) < 10f)
+        {
+            playerClose = true;
+        } else
+        {
+            rb.rotation = -90f;
+            playerClose = false;
+        }
+
+        if (playerClose)
+        {
+            Follow();
+        }
+    }
+
+
+    private void Follow()
     {
         Vector3 direction = player.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
